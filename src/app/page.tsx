11d4385678bf15b022aecc93df0a7ceb99d3c601
h0ngwon <w___on@naver.com>
2024-10-23 22:09:32 +0900

    feat: pie chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,116 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import * as React from "react";
+import { Label, Pie, PieChart } from "recharts";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+	ChartConfig,
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent,
+} from "@/components/ui/chart";
+
+export const description = "A donut chart with text";
+
+const chartData = [
+	{ month: "1월", userCount: 1800, fill: "var(--color-jan)" },
+	{ month: "2월", userCount: 200, fill: "var(--color-feb)" },
+	{ month: "3월", userCount: 287, fill: "var(--color-mar)" },
+	{ month: "4월", userCount: 173, fill: "var(--color-apr)" },
+	{ month: "5월", userCount: 190, fill: "var(--color-may)" },
+];
+
+const chartConfig = {
+	visitors: {
+		label: "Visitors",
+	},
+	jan: {
+		label: "1월",
+		color: "hsl(var(--chart-1))",
+	},
+	feb: {
+		label: "2월",
+		color: "hsl(var(--chart-2))",
+	},
+	mar: {
+		label: "3월",
+		color: "hsl(var(--chart-3))",
+	},
+	apr: {
+		label: "4월",
+		color: "hsl(var(--chart-4))",
+	},
+	may: {
+		label: "5월",
+		color: "hsl(var(--chart-5))",
+	},
+} satisfies ChartConfig;
+
+export default function Page() {
+	const totalVisitors = React.useMemo(() => {
+		return chartData.reduce((acc, curr) => acc + curr.userCount, 0);
+	}, []);
+
+	return (
+		<Card className="flex flex-col">
+			<CardHeader className="items-center pb-0">
+				<CardTitle>Pie Chart</CardTitle>
+			</CardHeader>
+			<CardContent className="flex-1 pb-0">
+				<ChartContainer
+					config={chartConfig}
+					className="mx-auto aspect-square max-h-[250px]"
+				>
+					<PieChart>
+						<ChartTooltip
+							cursor={false}
+							content={<ChartTooltipContent hideLabel />}
+						/>
+						<Pie
+							data={chartData}
+							dataKey="userCount"
+							nameKey="month"
+							innerRadius={60}
+							strokeWidth={5}
+						>
+							<Label
+								content={({ viewBox }) => {
+									if (
+										viewBox &&
+										"cx" in viewBox &&
+										"cy" in viewBox
+									) {
+										return (
+											<text
+												x={viewBox.cx}
+												y={viewBox.cy}
+												textAnchor="middle"
+												dominantBaseline="middle"
+											>
+												<tspan
+													x={viewBox.cx}
+													y={viewBox.cy}
+													className="fill-foreground text-3xl font-bold"
+												>
+													{`${totalVisitors.toLocaleString()}명`}
+												</tspan>
+												<tspan
+													x={viewBox.cx}
+													y={(viewBox.cy || 0) + 24}
+													className="fill-muted-foreground"
+												>
+													방문
+												</tspan>
+											</text>
+										);
+									}
+								}}
+							/>
+						</Pie>
+					</PieChart>
+				</ChartContainer>
+			</CardContent>
+		</Card>
+	);
 }
